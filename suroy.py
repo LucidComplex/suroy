a45012b382c3a0a763bfe717f7483ca30fc0fc5b
Eli Tan <eliedrian1996@gmail.com>
2016-05-20 00:47:22 +0800

    work on prompting

--- suroy.py
+++ suroy.py
@@ -11,3 +11,36 @@ def load_rooms(directory):
             room_dict[room_data.name[:-4]] = room.Room(room_data)
 
     return room_dict
+
+def main():
+    print('Loading game data.')
+    rooms = load_rooms('rooms')
+    room = rooms['begin']
+    start_game(rooms)
+
+def start_game(rooms):
+    current_room = rooms['begin']
+    while True:
+        print_room(current_room, True)
+        prompt()
+
+def prompt():
+    print('>>>', end=' ')
+    command = input()
+
+def print_room(room, first=False):
+    print(room.title)
+    if first:
+        print(room.intro)
+    print(room.desc)
+    if room.items:
+        print('Items:')
+        for name, count in room.items.items():
+            print(count, ' - ', name)
+        print()
+    print('Exits:')
+    for direction, name in room.exits.items():
+        print(str.upper(direction), ' - ', name)
+
+if __name__ == '__main__':
+    main()
